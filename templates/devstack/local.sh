e78df8913ec2784ac6cfc0a54bc3ebb1cfab12a8
gandrei95 <andreign95@gmail.com>
2018-11-02 12:49:15 +0200

    changed echo statements

--- templates/devstack/local.sh
+++ templates/devstack/local.sh
@@ -5,8 +5,8 @@ echo "Before updating nova flavors:"
 openstack flavor list
 
 nova flavor-delete 42 || echo "Flavor 42 not found"
-nova flavor-delete 84 || echo "Flavor 42 not found"
-nova flavor-delete 451 || echo "Flavor 42 not found"
+nova flavor-delete 84 || echo "Flavor 84 not found"
+nova flavor-delete 451 || echo "Flavor 451 not found"
 
 nova flavor-create m1.nano 42 256 1 1
 nova flavor-create m1.micro 84 256 2 1
